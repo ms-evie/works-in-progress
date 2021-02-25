022a022c6d5385ba528f2c8181eb61ae668627ba
ms-evie <78895253+ms-evie@users.noreply.github.com>
2021-02-24 19:24:48 -0800

    Delete cas222/project/includes directory

--- cas222/project/includes/header.inc.html.php
+++
@@ -1,4 +0,0 @@
-<?php define('BASE_URL', '/cas222/project/'); ?>
-<header>
-    <img src="<?php echo BASE_URL ?>images/ace1.jpg" alt="Ace in the Hole" width="920" height="290">
-</header>
--- cas222/project/includes/nav.inc.html.php
+++
@@ -1,9 +0,0 @@
-<nav>
-      <ul class="menu">
-        <li><a href="<?php echo BASE_URL; ?>">Home</a></li>
-        <li><a href="<?php echo BASE_URL; ?>contact">Contact</a></li>
-        <li><a href="<?php echo BASE_URL; ?>registration">Registration</a></li>
-        <li><a href="<?php echo BASE_URL; ?>information">Information</a></li>
-        <li><a href="<?php echo BASE_URL; ?>faq">FAQ</a></li>  
-      </ul>
-</nav>
