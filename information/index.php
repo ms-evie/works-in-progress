e3697674696c48e6de8412cfe69eddd5d6257615
ms-evie <78895253+ms-evie@users.noreply.github.com>
2021-03-08 21:38:03 -0800

    Add files via upload

---
+++ information/index.php
@@ -0,0 +1,28 @@
+<?php
+
+// Edit or Replace this try/catch statement to work with the current PHT configuration
+
+require '../includes/db.inc.html.php';
+
+try
+{
+  $sql = 'SELECT * FROM information';
+  $result = $pdo->query($sql);
+}
+catch (PDOException $e)
+{
+  $error = 'Error fetching page: ' . $e->getMessage();
+  include '../includes/error.html.php';
+  exit();
+}
+
+while ($row = $result->fetch())
+{
+  $contents[] = array(
+      'title' => $row['title'],
+      'description' => $row['description'],
+      );
+}
+
+include 'information.html.php';
+?>
---
+++ information/information.html.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ace in the Hole Multisport Events</title>
+  <meta name="description" content="Ace in the Hole Multisport Events is an event company that hosts multisport athletic events.">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="../css/reset.css" rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link href="../css/navigation.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div id="wrapper">
+    <?php include '../includes/nav.inc.html.php'; ?>
+        <main>
+            <h1>Information</h1>
+                <?php foreach ($contents as $content): ?>
+            <h2><?php echo htmlspecialchars($content['title'], ENT_QUOTES, 'UTF-8'); ?></h2>
+                 <?php echo $content['description']; ?>
+                <?php endforeach; ?> 
+        </main>
+    <?php include '../includes/footer.inc.html.php'; ?>
+  </div>
+    
+</body>
+</html>
