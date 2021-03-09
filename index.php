15b0e125338181dde5b9d65f9731226cbf3ba394
ms-evie <78895253+ms-evie@users.noreply.github.com>
2021-03-08 21:37:18 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-  $pdo = new PDO('mysql:host=localhost;dbname=evelynwe_project', 'user2', 'cxXyaZi8oSnGUyD5');
+  $pdo = new PDO('mysql:host=localhost;dbname=evelynwe_project', 'evelynwe_project_user', 'cxXyaZi8oSnGUyD5');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->exec('SET NAMES "utf8"');
 }
@@ -34,3 +34,4 @@ while ($row = $result->fetch())
 }
 
 include 'home/home.html.php';
+?>
