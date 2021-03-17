f350a44ab9bda172e8c426e152746993b7b89761
ms-evie <78895253+ms-evie@users.noreply.github.com>
2021-03-16 22:34:01 -0700

    Add files via upload

--- contact/contact.html.php
+++ contact/contact.html.php
@@ -18,24 +18,34 @@
     <div id="wrapper">
             <?php include '../includes/nav.inc.html.php'; ?>
         <main>
-        <h1>Contact Us</h1>    
-        <h2>Do you have questions? We'd love to hear from you!</h2>
+        <h1>Contact Us</h1><br>    
+            
+        <img src="../images/Pexels_Pixabay.jpg" alt="Riders racing" style="width:100% ;height:80%;">
+            
+        <h2>Do you have questions? We'd love to hear from you! You can also sign up for our newsletter below!</h2>
+            
       <form method="post" action=" " id="inquiryForm">
-        <input type="text" name="honeypot" id="honeypot" value="" alt="if you fill this field out then your email will not be sent"/>
-        <label for="myName">Name:</label>
+       
+          <input type="text" name="honeypot" id="honeypot" value="" alt="if you fill this field out then your email will not be sent"/>
+        
+          <label for="myName">Name:</label>
           <input type="text" name="myName" id="myName">
-        <label for="myEmail">E-mail:</label>
+        
+          <label for="myEmail">E-mail:</label>
           <input type="text" name="myEmail" id="myEmail">
-        <label for="myQuestion">Question or Comment:</label>
+        
+          <label for="myQuestion">Question or Comment:</label>
           <textarea form="inquiryForm" rows="15" cols="30" name="myQuestion" id="myQuestion"></textarea>
-        <label for="role">Role:</label>
+        
+          <label for="role">Role:</label>
         <select size="1" name="role" id="role">
           <option>Choose Your Role</option>
           <option value="athlete">Athlete</option>
           <option value="volunteer">Volunteer</option>
           <option value="interested">Interested Party</option>    
         </select>
-        <input id="mySubmit" type="submit" value="Submit">
+        
+          <input id="mySubmit" type="submit" value="Submit">
       </form>
         
     </main>
